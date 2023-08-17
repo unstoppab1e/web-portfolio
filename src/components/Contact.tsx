8a91734e198ba73f2cd8aeb90758bbbb78f31d4c
unstoppab1e <unstoppab1e@users.noreply.github.com>
2023-08-17 11:21:18 +0900

    fixed: gap in contacts

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -32,11 +32,11 @@ export default function Contact() {
   ];
 
   return (
-    <div className="pt-16">
+    <div className="flex flex-col gap-4 pt-16">
       <h3 className='font-inter font-title italic text-xl text-black dark:text-white pt-4 pb-4'>
         Contact
       </h3>
-      <div className="h-full flex-col gap-x-4">
+      <div className="flex flex-col gap-4 pb-4">
         {contacts && contacts.map((contact, index) => {
           return (
             <>
