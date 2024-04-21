de791959fbc37537139287e994b5ea889d0b35d4
unstoppab1e <unstoppab1e@users.noreply.github.com>
2024-04-22 02:45:58 +0900

    fix: first page background and theme button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,59 @@
-"use client";
-import BreakLines from "@/components/BreakLines";
-import Contact from "@/components/Contact";
-import Description from "@/components/Description";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import Now from "@/components/Now";
-import Overview from "@/components/Overview";
-import Projects from "@/components/Projects";
-import Stack from "@/components/Stack";
-import { DM_Mono } from "next/font/google";
-import { useRef } from "react";
+'use client';
+import BreakLines from '@/components/BreakLines';
+import Contact from '@/components/Contact';
+import Description from '@/components/Description';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import Now from '@/components/Now';
+import Overview from '@/components/Overview';
+import Projects from '@/components/Projects';
+import Stack from '@/components/Stack';
+import { DM_Mono } from 'next/font/google';
+import { useRef } from 'react';
 
 const mono = DM_Mono({
-  subsets: ["latin"],
+  subsets: ['latin'],
   weight: '400',
   variable: '--dm-mono',
-})
-
-
+});
 
 const Home: React.FC = () => {
-  const sectionRef = useRef<null | HTMLDivElement>(null); 
+  const sectionRef = useRef<null | HTMLDivElement>(null);
   const nowRef = useRef<null | HTMLDivElement>(null);
   const stackRef = useRef<null | HTMLDivElement>(null);
   const contactRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToSection = (sectionId:string) => {
-    if (sectionId==='projects' && sectionRef.current) {
+  const scrollToSection = (sectionId: string) => {
+    if (sectionId === 'projects' && sectionRef.current) {
       sectionRef.current.scrollIntoView({ behavior: 'smooth' });
-    } else if(sectionId==='now' && nowRef.current){
+    } else if (sectionId === 'now' && nowRef.current) {
       nowRef.current.scrollIntoView({ behavior: 'smooth' });
-    } else if(sectionId==='stack' && stackRef.current){
+    } else if (sectionId === 'stack' && stackRef.current) {
       stackRef.current.scrollIntoView({ behavior: 'smooth' });
-    } else if(sectionId==='contact' && contactRef.current){
+    } else if (sectionId === 'contact' && contactRef.current) {
       contactRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
   return (
-    <main className="flex flex-col font-mono text-base max-w-2xl mx-auto pt-14 pb-24 px-4 sm:px-6 lg:px-8">
+    <main className='flex flex-col font-mono text-base max-w-2xl mx-auto pt-14 pb-24 px-4 sm:px-6 lg:px-8'>
       <Navbar />
-      <div className={`${mono.variable} dark:bg-[#111215] dark:text-[#A1A1AA] h-full selection:bg-gray-50 dark:selection:bg-gray-800`}>
+      <div
+        className={`${mono.variable} dark:text-[#A1A1AA] h-full selection:bg-gray-50 dark:selection:bg-gray-800`}
+      >
         <Description />
-        <Overview scrollToSection={scrollToSection}/>
-        <Projects sectionRef={sectionRef}/>
+        <Overview scrollToSection={scrollToSection} />
+        <Projects sectionRef={sectionRef} />
         <BreakLines />
-        <Now nowRef={nowRef}/>
+        <Now nowRef={nowRef} />
         <BreakLines />
-        <Stack stackRef={stackRef}/>
+        <Stack stackRef={stackRef} />
         <BreakLines />
-        <Contact contactRef={contactRef}/>
+        <Contact contactRef={contactRef} />
         <BreakLines />
       </div>
       <Footer />
     </main>
-  )
-}
+  );
+};
 
 export default Home;
